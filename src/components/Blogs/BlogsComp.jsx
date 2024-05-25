a4db6f8a8bfcf027b8a57e77d8ab44c490bcb56b
Rana Usman <usman4243ch@gmail.com>
2024-05-25 02:14:26 -0700

    New Changes

--- src/components/Blogs/BlogsComp.jsx
+++ src/components/Blogs/BlogsComp.jsx
@@ -40,7 +40,7 @@ const BlogsComp = () => {
       <div className="dark:bg-gray-900 dark:text-white py-10">
         <section data-aos="fade-up" className="container ">
           <h1 className=" my-8 border-l-8 border-primary/50 py-2 pl-2 text-3xl font-bold">
-            Our Latest Blogs
+            Our Services
           </h1>
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-3">
             {BlogsData.map((item) => (
--- src/components/Places/Places.jsx
+++ src/components/Places/Places.jsx
@@ -10,44 +10,41 @@ import Img6 from "../../assets/places/place6.jpg";
 const PlacesData = [
   {
     img: Img1,
-    title: "Boat",
-    location: "USA",
-    description: "lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    title: "Hunza",
+    location: "Hunza (Gilgit-Baltistan)",
+    description: "Hunza Valley is a stunning place in Pakistan with beautiful mountains, clear rivers, and friendly people.",
     price: 6700,
-    type: "Cultural Relax",
+    type: "Tourism",
   },
   {
     img: Img2,
-    title: "Taj Mahal",
-    location: "India",
-    description:
-      "The Taj Mahal is an ivory-white marble mausoleum on the south bank of the river Yamuna in the Indian city of Agra.",
+    title: "Skardu",
+    location: "Skardu (Gilgit-Baltistan)",
+    description: "Skardu is a beautiful place in Pakistan known for its towering mountains, clear lakes, and warm hospitality.",
     price: 6700,
-    type: "Cultural Relax",
+    type: "Tourism",
   },
   {
     img: Img3,
-    title: "Underwater",
-    location: "US",
-    description:
-      "The Taj Mahal is an ivory-white marble mausoleum on the south bank of the river Yamuna in the Indian city of Agra.",
+    title: "Fairy Meddows",
+    location: "Gilgit-Baltistan",
+    description: "Fairy Meadows is a magical spot in Pakistan, offering stunning views of Nanga Parbat and a serene atmosphere.",
     price: 6200,
-    type: "Cultural Relax",
+    type: "Tourism",
   },
   {
     img: Img4,
-    title: "Sydney",
-    location: "USA",
-    description: "lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    title: "Sawat kalaam",
+    location: "Khyber Pakhtunkhwa",
+    description: "Swat Valley is a picturesque destination in Pakistan, renowned for its lush green valleys and crystal-clear rivers. ",
     price: 6700,
     type: "Cultural Relax",
   },
   {
     img: Img5,
-    title: "Los Angeles",
-    location: "United states",
-    description:
-      "The Taj Mahal is an ivory-white marble mausoleum on the south bank of the river Yamuna in the Indian city of Agra.",
+    title: "Kashmir",
+    location: "Kashmir",
+    description: "Kashmir is a mesmerizing region in Pakistan, famed for its breathtaking landscapes, serene lakes, and vibrant culture.",
     price: 6700,
     type: "Cultural Relax",
   },
@@ -68,7 +65,7 @@ const Places = ({ handleOrderPopup }) => {
       <div className="dark:bg-gray-900 dark:text-white bg-gray-50 py-10">
         <section data-aos="fade-up" className="container ">
           <h1 className=" my-8 border-l-8 border-primary/50 py-2 pl-2 text-3xl font-bold">
-            Best Places to Visit
+            Top Destinations to Explore in Pakistan
           </h1>
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
             {PlacesData.map((item, index) => (
